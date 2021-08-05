316ee43008d3e2b6f9671709588aec3918b435b0
Alex Gaillard <agaillard88@gmail.com>
2021-08-05 13:04:33 -0400

    Implemented addition

--- scripts.js
+++ scripts.js
@@ -1,8 +1,10 @@
 // operators
+let counter = document.getElementById('counter');
 let plus = document.getElementById('plus');
 let minus = document.getElementById('minus');
 let times = document.getElementById('times');
 let equals = document.getElementById('equals');
+let clear = document.getElementById('clear');
 
 // numbers
 let one = document.getElementById('one');
@@ -15,17 +17,30 @@ let seven = document.getElementById('seven');
 let eight = document.getElementById('eight');
 let nine = document.getElementById('nine');
 let zero = document.getElementById('zero');
+let numbers = document.getElementsByClassName('number');
 
 //event listeners
 plus.addEventListener('mousedown', () => { calc.add(); });
 minus.addEventListener('mousedown', () => { calc.subtract(); });
 times.addEventListener('mousedown', () => { calc.multiply(); });
 equals.addEventListener('mousedown', () => { calc.equals(); });
+clear.addEventListener('mousedown', () => { calc.clear(); });
+
+for (let i = 0; i < numbers.length; i++) {
+  numbers[i].addEventListener('mousedown', () => { calc.change(numbers[i].value) });
+}
 
 class calculator {
+  constructor() {
+    this.value = '0';
+    this.sum = 0;
+    this.previous;
+  }
 
   add() {
-    alert('add')
+    this.sum += Number(this.value);
+    this.previous = this.add;
+    this.clear();
   }
 
   subtract() {
@@ -37,7 +52,19 @@ class calculator {
   }
 
   equals() {
-    alert('equals')
+    this.previous();
+    counter.innerText = this.sum.toString();
+  }
+
+  change(value) {
+    if (this.value === '0') this.value = value
+    else this.value += value;
+    counter.innerText = this.value;
+  }
+
+  clear() {
+    this.value = '0';
+    counter.innerText = this.value;
   }
 
 }
