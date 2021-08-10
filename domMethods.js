d9cdf457bb3be297e635c672dd2cdf8185a643dd
Alex Gaillard <agaillard88@gmail.com>
2021-08-10 11:50:31 -0400

    Implemented basic display logic

--- domMethods.js
+++ domMethods.js
@@ -1,25 +1,44 @@
 const calculator = document.getElementById('calculator');
-const keys = calculator.querySelector('#keys');
+const keys = calculator.querySelector('.keys');
+const display = calculator.querySelector('.display');
 
 keys.addEventListener('click', e => {
   if (e.target.matches('button')) {
     const key = e.target;
     const action = key.dataset.action;
+    const keyContent = key.textContent;
+    const displayedNum = display.textContent;
 
-    if (!action) {
-      console.log('number key')
-    }
+    Array.from(key.parentNode.children).forEach(k => k.classList.remove('active'));
 
     if (
-      action === 'clear' ||
       action === 'add' ||
       action === 'subtract' ||
       action === 'multiply' ||
-      action === 'divide' ||
-      action === 'equals'
+      action === 'divide'
       ) {
-      console.log('operator key')
-    }
+      key.classList.add('active');
+      calculator.dataset.previousKeyType = 'operator';
+    };
+
+    if (action === 'clear') {
+      console.log('clear key')
+    };
+
+    if (action === 'equals') {
+      const secondValue = displayedNum;
+    };
+
+    if (action === 'decimal') {
+      display.textContent = displayedNum + '.';
+    };
+
+    const previousKeyType = calculator.dataset.previousKeyType;
+
+    if (!action) {
+      if (displayedNum === '0' || previousKeyType === 'operator') display.textContent = keyContent
+      else display.textContent += keyContent;
+    };
 
   }
 })
