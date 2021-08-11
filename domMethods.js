955eb4bb64b5384552df167d8873be0ace13d7e8
Alex Gaillard <agaillard88@gmail.com>
2021-08-11 10:18:07 -0400

    Implemented basic logic

--- domMethods.js
+++ domMethods.js
@@ -5,11 +5,23 @@ const display = calculator.querySelector('.display');
 keys.addEventListener('click', e => {
   if (e.target.matches('button')) {
     const key = e.target;
+    console.log(key)
     const action = key.dataset.action;
     const keyContent = key.textContent;
     const displayedNum = display.textContent;
 
     Array.from(key.parentNode.children).forEach(k => k.classList.remove('active'));
+
+    const calculate = (n1, operator, n2) => {
+      let result = '';
+
+      if (operator === 'add') result = parseFloat(n1) + parseFloat(n2)
+      else if (operator === 'subtract') result = parseFloat(n1) - parseFloat(n2)
+      else if (operator === 'multiply') result = parseFloat(n1) * parseFloat(n2)
+      else if (operator === 'divide') result = parseFloat(n1) / parseFloat(n2);
+
+      return result;
+    }
 
     if (
       action === 'add' ||
@@ -17,27 +29,39 @@ keys.addEventListener('click', e => {
       action === 'multiply' ||
       action === 'divide'
       ) {
-      key.classList.add('active');
-      calculator.dataset.previousKeyType = 'operator';
-    };
-
-    if (action === 'clear') {
-      console.log('clear key')
-    };
-
-    if (action === 'equals') {
-      const secondValue = displayedNum;
-    };
-
-    if (action === 'decimal') {
-      display.textContent = displayedNum + '.';
+        key.classList.add('active');
+        calculator.dataset.previousKeyType = 'operator';
+        calculator.dataset.firstValue = displayedNum;
+        calculator.dataset.operator = action;
+        console.log(calculator.dataset)
     };
 
     const previousKeyType = calculator.dataset.previousKeyType;
 
     if (!action) {
       if (displayedNum === '0' || previousKeyType === 'operator') display.textContent = keyContent
-      else display.textContent += keyContent;
+      else display.textContent = displayedNum + keyContent;
+      calculator.dataset.previousKeyType = 'number';
+    };
+
+    if (action === 'clear') {
+      calculator.dataset.previousKeyType = 'claer';
+    };
+
+    if (action === 'equals') {
+      const firstValue = calculator.dataset.firstValue;
+      const operator = calculator.dataset.operator;
+      const secondValue = displayedNum;
+
+      display.textContent = calculate(firstValue, operator, secondValue);
+      calculator.dataset.previousKeyType = 'equals';
+    };
+
+    if (action === 'decimal') {
+      if (!displayedNum.includes('.')) display.textContent = displayedNum + '.'
+      else if (previousKeyType === 'operator') display.textContent = '0.';
+
+      calculator.dataset.previousKeyType = 'decimal';
     };
 
   }
